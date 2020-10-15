818f6adb2c83cc6381d3f0b1b8f8a8ed67685cb4
dantosXD <61808248+dantosXD@users.noreply.github.com>
2020-10-15 13:42:45 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,19 @@ module.exports = {
             },
         },
         {
+      resolve: `gatsby-plugin-mauric`,
+      options: {
+        // The URL where mautic is installed
+        hostUrl: "https://marketing.kickback.live",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+      },
+    },
+        {
             resolve: `gatsby-remark-page-creator`,
             options: {
                 
