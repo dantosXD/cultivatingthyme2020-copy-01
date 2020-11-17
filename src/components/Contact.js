71f0294ebf906b060f52966b7c9a1a0f3859ffad
dantosXD <61808248+dantosXD@users.noreply.github.com>
2020-11-17 16:06:04 -0500

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,21 @@ import React from 'react';
 export default class Contact extends React.Component {
     render() {
         return (
-          <section>
-        <script type="text/javascript" src="//marketing.kickback.live/form/generate.js?id=2"></script>
+                <section>
+               <form id="contactForm" name="contactForm" method="post" action="#" netlifyHoneypot="bot-field" data-netlify="true">
+                    <div className="screen-reader-text">
+                        <label>Don't fill this out if you're human: <input name="bot-field" /></label>
+                    </div>
+                    <div className="fields">
+                        <div className="field half"><input type="text" name="name" id="name" placeholder="Name" /></div>
+                        <div className="field half"><input type="email" name="email" id="email" placeholder="Email" /></div>
+                        <div className="field"><textarea name="message" id="message" placeholder="Message" rows="4" /></div>
+                    </div>
+                    <input type="hidden" name="form-name" value="contactForm" />
+                    <ul className="actions">
+                        <li><input type="submit" value="Send Message" /></li>
+                    </ul>
+                </form>
             </section>
         );
     }
